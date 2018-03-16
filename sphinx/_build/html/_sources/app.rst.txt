b65199ac0216ef5f6f179a89590a54d06d6160b2
rishabh-joshi <rishabhjoshi15@gmail.com>
2018-03-16 02:31:17 -0500

    Add Sphinx Documentation

---
+++ sphinx/_build/html/_sources/app.rst.txt
@@ -0,0 +1,8 @@
+Web Application
+===============
+
+
+.. automodule:: create_db
+   :members:
+.. automodule:: showtable
+   :members:
--- sphinx/conf.py
+++ sphinx/conf.py
@@ -17,9 +17,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.append('../develop/src/models')
+sys.path.append('../app')
 
 
 # -- General configuration ------------------------------------------------
@@ -31,7 +33,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
