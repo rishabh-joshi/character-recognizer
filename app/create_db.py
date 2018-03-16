fcda19bbac68c94f7be21bad33fef904499bb1a4
rishabh-joshi <rishabhjoshi15@gmail.com>
2018-03-16 02:42:44 -0500

    Add docstrings

--- app/create_db.py
+++ app/create_db.py
@@ -7,15 +7,6 @@ class Prediction(db.Model):
 
     id = db.Column(db.Integer,primary_key=True)
     character = db.Column(db.String(1),unique=False,nullable=False)
-    # area = db.Column(db.String(15),unique=False,nullable=False)
-    # genre = db.Column(db.String(30),unique=False,nullable=False)
-    # avg_temp = db.Column(db.Float,unique=False,nullable=False)
-    # low_temp = db.Column(db.Float,unique=False,nullable=False)
-    # precip = db.Column(db.Float,unique=False,nullable=False)
-    # wind = db.Column(db.Float,unique=False,nullable=False)
-    # sunlight = db.Column(db.Float,unique=False,nullable=False)
-    # visitor_hist = db.Column(db.Integer,unique=False,nullable=False)
-    # visitor_pred = db.Column(db.Integer,unique=False,nullable=False)
 
     def __repr__(self):
         return('<Prediction Hist %r>'% id)
--- app/showtable.py
+++ app/showtable.py
@@ -1,11 +1,36 @@
 from flask_table import Table, Col
 
 class dataTable(Table):
+    """A class to store the entries in a row of a database table.
+	
+	The table in the database would store the predicted values of the
+	character drawn by the user. This class stores the entries in one
+	row of that table. This class inherits from the Table class in
+	flask_table module.
+
+    Attributes:
+        id (Col): Unique ID column in the table.
+        character (Col): Column storing the predicted user drawn character.
+
+    """
     id = Col("id")
     character = Col('character')
 
 class dataRow:
+    """A class to store a row in the table in the database.
+
+    Attributes:
+        id (str): Unique ID for each row in the table.
+        character (str): The predicted character drawn by the user.
+
+    """
     def __init__(self, row):
+        """Example of docstring on the __init__ method.
+
+        Args:
+            row (list): List id and character in one row.
+
+        """
         self.id = row[0]
         self.character = row[1]
 
