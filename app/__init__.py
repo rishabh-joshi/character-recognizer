f60ff3d22180213cc1f9c190865766c067dc31ff
rishabh-joshi <rishabhjoshi15@gmail.com>
2018-03-17 01:58:08 -0500

    Setup up config file and environment variables for accessing RDS database

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ def dbAddress(username,password,dbaddress,database):
     return 'postgresql://'+username+':'+password+'@'+dbaddress+'/'+database
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = dbAddress(get_credential()[0],get_credential()[1],get_credential()[2],get_credential()[3])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = dbAddress(get_credential()[0],get_credential()[1],get_credential()[2],get_credential()[3])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config.from_envvar('APP_SETTINGS', silent=False)
 db = SQLAlchemy(app)
 
--- app/get_credential.py
+++
@@ -1,8 +0,0 @@
-def get_credential():
-    file = open('config','r')
-    credential = file.read()
-    username = credential.split(",")[0]
-    password = credential.split(",")[1]
-    dbaddress = credential.split(",")[2]
-    database = credential.split(",")[3]
-    return [username,password,dbaddress,database]
