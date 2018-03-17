8c79ef35ad810c346a8aca722f212d4fe7d03755
rishabh-joshi <rishabhjoshi15@gmail.com>
2018-03-17 02:07:44 -0500

    Typo

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from get_credential import get_credential
-
-def dbAddress(username,password,dbaddress,database):
-    return 'postgresql://'+username+':'+password+'@'+dbaddress+'/'+database
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = dbAddress(get_credential()[0],get_credential()[1],get_credential()[2],get_credential()[3])
